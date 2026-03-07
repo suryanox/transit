8db380eeaee42898832a5c987ae1e1357a600acc
Sooraj <sooraj.crafts@gmail.com>
2026-03-07 23:37:43 +0800

    Delete src directory

--- src/src/components/index.ts
+++
@@ -1,7 +0,0 @@
-export { CopyButton } from "./CopyButton";
-export { CodeBlock } from "./CodeBlock";
-export { Sidebar } from "./Sidebar";
-export { RequestList } from "./RequestList";
-export { RequestDetails } from "./RequestDetails";
-export { ProxyHeader } from "./ProxyHeader";
-export { NoProxySelected, NoRequestSelected } from "./EmptyState";
